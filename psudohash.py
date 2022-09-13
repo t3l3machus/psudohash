dcb631317a097e6cd7e2011499751c7dfc07f417
Panagiotis Chartas <t3l3machus@protonmail.com>
2022-09-13 10:49:41 +0200

    Update psudohash.py

--- psudohash.py
+++ psudohash.py
@@ -103,12 +103,43 @@ if args.years:
 			
 
 def banner():
+	padding = '  '
 
-	pad = '   '
-	print('\n')
-	print(f'{pad}{LOGO}█▀▄{PRPL}░{LOGO}▄▀▀{PRPL}░{LOGO}█{PRPL}▒{LOGO}█{PRPL}░{LOGO}█▀▄{PRPL}░{LOGO}▄▀▄{PRPL2}░{LOGO}█▄█{PRPL}▒{LOGO}▄▀▄{PRPL2}░{LOGO}▄▀▀{PRPL}░{LOGO}█▄█')
-	print(f'{pad}{LOGO2}█▀{PRPL2}▒▒{LOGO2}▄██{PRPL}░{LOGO2}█▄█{PRPL2}▒{LOGO2}█▄▀{PRPL}░{LOGO2}▀▄▀{PRPL}▒{LOGO2}█{PRPL2}▒{LOGO2}█{PRPL}░{LOGO2}█▀█{PRPL}▒{LOGO2}▄██{PRPL2}▒{LOGO2}█{PRPL}▒{LOGO2}█{END}')
-	print('\t         Created by t3l3machus\n')
+	P = [[' ', '┌', '─', '┐'], [' ', '├','─','┘'], [' ', '┴',' ',' ']]
+	S = [[' ', '┌','─','┐'], [' ', '└','─','┐'], [' ', '└','─','┘']]
+	U = [[' ', '┬',' ','┬'], [' ', '│',' ','│'], [' ', '└','─','┘']]
+	D = [[' ', '┌','┬','┐'], [' ', ' ','│','│'], [' ', '─','┴','┘']]
+	O =	[[' ', '┌','─','┐'], [' ', '│',' ','│'], [' ', '└','─','┘']]
+	H = [[' ', '┐', ' ', '┌'], [' ', '├','╫','┤'], [' ', '┘',' ','└']]	
+	A = [[' ', '┌','─','┐'], [' ', '├','─','┤'], [' ', '┴',' ','┴']]
+	S = [[' ', '┌','─','┐'], [' ', '└','─','┐'], [' ', '└','─','┘']]
+	H = [[' ', '┬',' ','┬'], [' ', '├','─','┤'], [' ', '┴',' ','┴']]
+
+	banner = [P,S,U,D,O,H,A,S,H]
+	final = []
+	print('\r')
+	init_color = 37
+	txt_color = init_color
+	cl = 0
+
+	for charset in range(0, 3):
+		for pos in range(0, len(banner)):
+			for i in range(0, len(banner[pos][charset])):
+				clr = f'\033[38;5;{txt_color}m'
+				char = f'{clr}{banner[pos][charset][i]}'
+				final.append(char)
+				cl += 1
+				txt_color = txt_color + 36 if cl <= 3 else txt_color
+
+			cl = 0
+
+			txt_color = init_color
+		init_color += 31
+
+		if charset < 2: final.append('\n   ')
+
+	print(f"   {''.join(final)}")
+	print(f'{END}{padding}                        by t3l3machus\n')
 
 
 # ----------------( Base Settings )---------------- #
